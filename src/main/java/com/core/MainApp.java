8ef718f8d370daef8e874fd308957473d3167f0e
Daniele <daniele.cali95@gmail.com>
2018-06-17 11:46:25 +0200

    Adds drag to window and update pom.xml

--- src/main/java/com/core/MainApp.java
+++ src/main/java/com/core/MainApp.java
@@ -1,36 +1,70 @@
 package com.core;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.WeakEventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/** Main Application Class */
 public class MainApp extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    private double mouseDragDeltaX = 0;
+    private double mouseDragDeltaY = 0;
+    private EventHandler<MouseEvent> mousePressedHandler;
+    private EventHandler<MouseEvent> mouseDraggedHandler;
+    private WeakEventHandler<MouseEvent> weakMousePressedHandler;
+    private WeakEventHandler<MouseEvent> weakMouseDraggedHandler;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
+        log.info("Starting JavaFXMusicPlayer");
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
+        String fxmlFile = "/fxml/MusicPlayerGUI.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
+        Scene scene = new Scene(rootNode, 1520, 900);
         scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("Hello JavaFX and Maven");
+        stage.setTitle("Music Player");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        allowDrag(rootNode, stage);
         stage.show();
+    }
+
+    /**
+     * Allow the drag of the window
+     * @param node the parent node press
+     * @param stage the stage to drag
+     */
+    protected void allowDrag(Node node, Stage stage) {
+        mousePressedHandler = (MouseEvent event) -> {
+            mouseDragDeltaX = node.getLayoutX() - event.getSceneX();
+            mouseDragDeltaY = node.getLayoutY() - event.getSceneY();
+        };
+        weakMousePressedHandler = new WeakEventHandler<>(mousePressedHandler);
+        node.setOnMousePressed(weakMousePressedHandler);
+
+        mouseDraggedHandler = (MouseEvent event) -> {
+            stage.setX(event.getScreenX() + mouseDragDeltaX);
+            stage.setY(event.getScreenY() + mouseDragDeltaY);
+        };
+        weakMouseDraggedHandler = new WeakEventHandler<>(mouseDraggedHandler);
+        node.setOnMouseDragged(weakMouseDraggedHandler);
     }
 }
